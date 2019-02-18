573984e7d30c0af05edc1131cc992bb5d8c47652
Vasconcelos <vasconcelos.lucas@eb.mil.br>
2019-02-18 20:58:07 -0300

    Primeiro Commit

---
+++ src/main/java/com/maisvida/JobUseCase/JobUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.maisvida.JobUseCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobUseCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maisvida/JobUseCase/JobUseCaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maisvida.JobUseCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobUseCaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
