962c6fb04f22d15c55f1247a00a5320a6a1c90e5
Vasconcelos <vasconcelos.lucas@eb.mil.br>
2019-02-18 21:46:28 -0300

    Add model

--- src/main/java/com/maisvida/jobusecase/model/Job.java
+++ src/main/java/com/maisvida/jobusecase/model/Job.java
@@ -1,5 +1,51 @@
 package com.maisvida.jobusecase.model;
 
-public class Job {
+import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Job {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String name;
+	private boolean active;
+	private Job parentJob;
+	
+	@ManyToMany
+	private List<Task> tasks;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public Job getParentJob() {
+		return parentJob;
+	}
+	public void setParentJob(Job parentJob) {
+		this.parentJob = parentJob;
+	}
+	public List<Task> getTasks() {
+		return tasks;
+	}
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}	
 }
