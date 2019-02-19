89a692a11a0c0af883d5033446940e268785e73e
Vasconcelos <vasconcelos.lucas@eb.mil.br>
2019-02-18 21:05:47 -0300

    Correção de nome

--- src/main/java/com/maisvida/jobusecase/JobUseCaseApplication.java
+++ src/main/java/com/maisvida/jobusecase/JobUseCaseApplication.java
@@ -1,4 +1,4 @@
-package com.maisvida.JobUseCase;
+package com.maisvida.jobusecase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/maisvida/jobusecase/model/Job.java
@@ -0,0 +1,5 @@
+package com.maisvida.jobusecase.model;
+
+public class Job {
+
+}
